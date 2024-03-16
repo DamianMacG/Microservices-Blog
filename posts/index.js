a9c539c6f5a7e558da81424752710e5b95ddc6ce
damiang <damiangildea12@gmail.com>
2024-03-16 10:16:41 +0000

    Added delete route

--- posts/index.js
+++ posts/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
+const cors = require("cors");
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const posts = {};
 
@@ -22,6 +24,18 @@ app.post("/posts", (req, res) => {
   };
 
   res.status(201).send(posts[id]);
+});
+
+app.delete("/posts/:id", (req, res) => {
+  const { id } = req.params;
+
+  if (!posts[id]) {
+    return res.status(404).json({ error: "Post not found" });
+  }
+
+  delete posts[id];
+
+  res.status(200).send({ message: "Post deleted successfully" });
 });
 
 app.listen(4000, () => {
